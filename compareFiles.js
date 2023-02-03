4ca97058e3ea027b78d5c594893b645af879ae2c
maxeq <maxeq1@gmail.com>
2023-02-03 21:20:54 +0100

    up

--- compareFiles.js
+++ compareFiles.js
@@ -1,14 +1,14 @@
+const { count } = require('console');
 const fs = require('fs');
 const path = require('path');
+const stream = require('stream');
+const util = require('util');
 
 async function compareFiles(folder1, folder2, outputDirectory) {
   try {
     if (!fs.existsSync(outputDirectory)) {
       fs.mkdirSync(outputDirectory);
     }
-
-    const uniqueNames1 = [];
-    const uniqueNames2 = [];
     const fileNames1 = await new Promise((resolve, reject) => {
       fs.readdir(folder1, (error, files) => {
         if (error) {
@@ -28,27 +28,52 @@ async function compareFiles(folder1, folder2, outputDirectory) {
       });
     });
 
-    for (const fileName1 of fileNames1) {
-      if (!fileNames2.includes(fileName1)) {
-        uniqueNames1.push({ folder: folder1, fileName: fileName1 });
-      }
-    }
+    const compareStream = new stream.Transform({
+      objectMode: true,
+      transform(fileName1, encoding, callback) {
+        if (!fileNames2.includes(fileName1)) {
+          this.push({ folder: folder1, fileName: fileName1 });
+        }
+        callback();
+      },
+    });
 
-    for (const fileName2 of fileNames2) {
-      if (!fileNames1.includes(fileName2)) {
-        uniqueNames2.push({ folder: folder2, fileName: fileName2 });
-      }
-    }
-    const results = uniqueNames1.concat(uniqueNames2);
+    const compareStream2 = new stream.Transform({
+      objectMode: true,
+      transform(fileName2, encoding, callback) {
+        if (!fileNames1.includes(fileName2)) {
+          this.push({ folder: folder2, fileName: fileName2 });
+        }
+        callback();
+      },
+    });
+
+    fileNames1.forEach((fileName) => compareStream.write(fileName));
+    compareStream.end();
+
+    fileNames2.forEach((fileName) => compareStream2.write(fileName));
+    compareStream2.end();
+
+    const results = [];
+    compareStream.on('data', (data) => results.push(data));
+    compareStream2.on('data', (data) => results.push(data));
+
+    await util.promisify(stream.finished)(compareStream);
+    await util.promisify(stream.finished)(compareStream2);
+  
     const filePath = path.join(outputDirectory, 'results.json');
     fs.writeFileSync(filePath, JSON.stringify(results, null, 2));
-    return results;
+
+    console.log(`Unique files in folder1: ${results.filter(result => result.folder === folder1).length}`);
+    console.log(`Unique files in folder2: ${results.filter(result => result.folder === folder2).length}`);
+    console.log(`Total unique files: ${results.length}`);
     
+    return results;
   } catch (error) {
     console.error(error);
     return [];
-    
   }
 }
 
 compareFiles('D:\\1', 'D:\\2', 'D:\\output').catch(error => console.error(error));
+
